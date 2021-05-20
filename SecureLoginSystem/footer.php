3fa60292672e9c7a26fe5f3cae9a5022c7f9c76a
itsdavid96 <70228813+itsdavid96@users.noreply.github.com>
2021-05-20 11:13:37 -0700

    Delete SecureLoginSystem directory

--- SecureLoginSystem/footer.php
+++
@@ -1,9 +0,0 @@
-<footer>
-    <p>&copy; 2021 - CIT 481 Team GATE</p>
-</footer>
-
-<script src="https://www.google.com/recaptcha/api.js" async defer></script>
-<!-- <script src="bootstrap/bootstrap.min.js"></script> -->
-</body>
-
-</html>
